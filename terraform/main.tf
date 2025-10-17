3e5f946256d9fc6cf2426659de434c628b43debf
yomaxx <max.valkenburg@hotmail.com>
2025-10-17 23:18:37 +0000

    added immich-lxc terraform, added optional mount point

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,21 @@ variable LXC {
     name        = string
     vmid        = number
     cores       = number
-    memory      = number # in GB
+    memory      = number  # in GB
     storage_size = number
     ip_address  = string
     unprivileged  = optional(bool, true)
 
+    mount_point = optional(object({
+      mp       = string   # Mount point inside the container, e.g. "/mnt/data"
+      storage  = string   # Storage location, e.g. "/mnt/data"
+    }))
+
   }))
     default = [
-        { name = "n8n-LXC", vmid = 115, cores = 2, memory = 8, storage_size = 20, ip_address = "192.168.10.226/24"}
+        { name = "n8n-LXC", vmid = 115, cores = 2, memory = 4, storage_size = 20, ip_address = "192.168.10.226/24"},
+        { name = "immich-LXC", vmid = 116, cores = 4, memory = 6, storage_size = 10, ip_address = "192.168.10.227/24", 
+          mount_point = { mp = "/opt/immich/upload", storage = "/mnt/immich" }}
     ]
 }
 
@@ -44,6 +51,17 @@ resource "proxmox_lxc" "lxc" {
     bridge = "vmbr0"
     gw = "192.168.10.1"
     ip = each.value.ip_address
+  }
+
+  dynamic "mountpoint" {
+    for_each = each.value.mount_point == null ? [] : [each.value.mount_point]
+
+    content {
+      key     = "0"
+      slot    = "0"
+      mp      = mountpoint.value.mp
+      volume = mountpoint.value.storage
+    }
   }
 }
 
